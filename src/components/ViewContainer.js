cc95746809a063f81193c116bb81b3621b6c3d1e
Maria Luisa <maria.luisa@thoughtworks.com>
2020-05-13 13:24:37 -0300

    Remove unnecessary code

--- src/components/ViewContainer.js
+++ src/components/ViewContainer.js
@@ -5,8 +5,8 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 const CustomSafeAreaView = styled(SafeAreaView)`
   background-color: ${(props) => props.theme.background};
   flex: 1;
-  padding-left: 8px;
-  padding-right: 8px;
+  padding-left: 24px;
+  padding-right: 24px;
 `;
 
 export default ({children}) => {
