7e301616c87465a8d7b3ca31b5d97e84f2a284ac
Maria Luisa <maria.luisa@thoughtworks.com>
2020-05-27 13:22:54 -0300

    Adding CreateNote in service

--- src/services/NoteService.js
+++ src/services/NoteService.js
@@ -1,10 +1,23 @@
 const URL_API = 'https://fake-keep-api.herokuapp.com';
 
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
+const headers = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
 };
 
 export const getNotes = (pagination = '') => {
-  return fetch(`${URL_API}/notes${pagination}`, requestOptions).then(response => response.json());
+  return fetch(`${URL_API}/notes${pagination}`, {
+    method: 'GET',
+    headers: headers,
+  }).then((response) => response.json());
+};
+
+export const createNote = (note) => {
+  return fetch(`${URL_API}/notes`, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(note),
+  })
+    .then((response) => response.json())
+    .then((data) => data);
 };
